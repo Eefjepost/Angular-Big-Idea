709097809e61cb864223d8901f1759c4cb00ae86
Eefje Post <eefje.post@ordina.nl>
2020-06-08 09:27:36 +0200

    first commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {inspect} from 'util';
+
 
 @Component({
   selector: 'app-home',
@@ -6,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public currentWindowWidth: number;
 
   constructor() { }
 
+
   ngOnInit(): void {
     window.scrollTo(0, 0);
+    this.currentWindowWidth = window.innerWidth;
   }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.currentWindowWidth = window.innerWidth;
+  }
+
+
+
 
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,6 +13,7 @@ export class ProjectsComponent implements OnInit {
 
 
   ngOnInit(): void {
+    window.scrollTo(0, 0);
   }
 
   onBack(): void {
